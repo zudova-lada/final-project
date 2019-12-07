a43afac751e73cbdcbeb2176b695c3fcbd16f4e6
Lada Zudova <zudova.lada@mail.ru>
2019-12-07 18:16:49 +0300

    no message

---
+++ ImageStructure+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ImageStructure+CoreDataClass.swift
+//  FinalProject
+//
+//  Created by Лада on 06/12/2019.
+//  Copyright © 2019 Лада. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ImageStructure)
+public class ImageStructure: NSManagedObject {
+
+}
